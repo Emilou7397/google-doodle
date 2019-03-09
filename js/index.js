4acbb981cc57127214ae17cec8c81fd5c1d15b18
Emily Cone <econe@eagles.ewu.edu>
2019-03-09 09:13:48 -0800

    Updated to V4

--- js/index.js
+++ js/index.js
@@ -1,63 +1,38 @@
-console.log("OK");
+console.log('OK');
 
-// Functions
-var bounce = function bounce() {
+// Teapot lid bounce
+
+var bounce = function() {
   $(".lid").addClass("bounce");
 
   setTimeout(function() {
     $(".lid").removeClass("bounce");
   }, 1000);
 };
+var bounce_shading = function() {
+  $(".lid_shading").addClass("bounce");
+
+  setTimeout(function() {
+    $(".lid_shading").removeClass("bounce");
+  }, 1000);
+};
+
+// Time function
 
 var time = function time() {
-  $("#light").css("top", "-20px");
+  $("#light_pull").css("top", "5px");
   setTimeout(function() {
-    $("#light").css("top", "-50px");
+    $("#light_pull").css("top", "-25px");
   }, 500);
+  
+  $('.night').toggleClass('visible');
+  $('.window_light').toggleClass('window_light_night');
+  $('.window_dark').toggleClass('window_dark_night');
+  $('.window_glass').toggleClass('window_glass_night');
+  $('body').toggleClass('night_background');
+}
 
-  $(".shadow").toggleClass("none");
-  $(".sill").toggleClass("sill_night");
-  $("body").toggleClass("night");
-  $("#stage").toggleClass("night");
-  $(".block-top").toggleClass("block-night");
-  $(".block-bottom").toggleClass("block-night");
+$(".teapot").mouseenter(bounce);
+$(".teapot_shading").mouseenter(bounce_shading);
 
-  $(".blue_highlight").toggleClass("blue_reg");
-  $(".blue_dark").toggleClass("blue_reg");
-  $(".blue_v_dark").toggleClass("blue_reg");
-  $(".tail").toggleClass("blue_tail");
-
-  $(".red_highlight").toggleClass("red_reg");
-  $(".red_dark").toggleClass("red_reg");
-  $(".red_v_dark").toggleClass("red_reg");
-
-  $(".yellow_highlight").toggleClass("yellow_dark");
-  $(".yellow_v_dark").toggleClass("yellow_dark");
-  $(".body").toggleClass("yellow_reg");
-  $(".body").toggleClass("yellow_dark");
-  $(".light").toggleClass("yellow_reg");
-
-  $(".green_highlight").toggleClass("green_reg");
-  $(".green_dark").toggleClass("green_reg");
-  $(".green_v_dark").toggleClass("green_reg");
-
-  $(".city_day").toggleClass("city_off");
-  $(".lights").toggleClass("lights_on");
-
-  $(".steam").toggleClass("none");
-  $(".steam2").toggleClass("none");
-
-  $(".window_dark").toggleClass("window_dark_night");
-  $(".window_light").toggleClass("window_light_night");
-  $(".window_glass").toggleClass("window_glass_night");
-};
-
-var windowToggle = function windowToggle() {
-  $("#window_front").toggleClass("window_open");
-};
-
-$("#teapot").mouseenter(bounce);
-
-$("#light").click(time);
-
-$("#window_front").click(windowToggle);
+$("#light_pull").click(time);
